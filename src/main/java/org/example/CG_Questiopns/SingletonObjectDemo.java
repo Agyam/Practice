4183f6d832278d76d0c300eab8cc6b35636e147c
KSAgyamNaik <agyamnaik1998@gmail.com>
2025-07-28 02:13:09 +0530

    singleton

--- src/main/java/org/example/CG_Questiopns/SingletonObjectDemo.java
+++ src/main/java/org/example/CG_Questiopns/SingletonObjectDemo.java
@@ -1,2 +1,17 @@
-package org.example.CG_Questiopns;public class SingletonObjectDemo {
+package org.example.CG_Questiopns;
+
+public class SingletonObjectDemo {
+    private static SingletonObjectDemo singletonObjectDemo;
+
+   private SingletonObjectDemo(){
+       System.out.println(" singleton object initialize ");
+   }
+
+    public static SingletonObjectDemo getInstance(){
+       if(singletonObjectDemo==null){
+           singletonObjectDemo=new SingletonObjectDemo();
+       }
+       return singletonObjectDemo;
+   }
+
 }
